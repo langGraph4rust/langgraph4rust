bb85c3339fa02efe19b5f77365617d3458e8285f
马腾佼 <matengjiao@bwcj.com>
2026-07-23 15:52:37 +0800

    1

--- src/core/agent_node.rs
+++ src/core/agent_node.rs
@@ -7,7 +7,7 @@ use crate::core::error::LangGraphError;
 pub trait AgentNode<S: AgentState> {
     fn apply(&self, state: &mut S) -> Result<HashMap<String, Value>, LangGraphError>;
     
-    fn fallback(&self, state: &mut S, error: &(dyn Error + Send + Sync)) -> Result<HashMap<String, Value>, LangGraphError> {
+    fn fallback(&self, state: &mut S, error: &(dyn Error)) -> Result<HashMap<String, Value>, LangGraphError> {
         Err(LangGraphError::NodeError(error.to_string().into()))
     }
 }
